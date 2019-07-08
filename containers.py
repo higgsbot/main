03324a74b2158111492a4c46bd384527e2a2c847
IOIIIO <101110@vivaldi.net>
2019-07-08 23:40:31 +0200

    Fix typo, add embed colour, disable Codetoken arg

--- containers.py
+++ containers.py
@@ -12,24 +12,29 @@ class Containers(commands.Cog, name="Container Plugin"):
         self.bot = bot
 
     @commands.command(name='code')
-    async def code(self, ctx, b:int, lang:str, *, code:str):
+    #async def code(self, ctx, b:int, lang:str, *, code:str):
+    async def code(self, ctx, lang:str, *, code:str):
         """Use this command to execute your code."""
         c = currency.check_balance(ctx.message.author)
-        if c >= b:
+        if c > 0:
             r = container.run_code(lang, code)
-            currency.remove_balance(ctx.message.author, b)
+            currency.remove_balance(ctx.message.author, 1)
             if r.chroot == False:
                 chr = "NOCHROOT"
             else:
                 chr = "CHROOT"
+            if r.cr == "":
+                clr = 0xff0000
+            else:
+                clr = 0x25df20
             if r.ccr == "":
-                embed=discord.Embed(title="Results of language {}".format(lang), color=0xee6d20)
+                embed=discord.Embed(title="Results of language {}".format(lang), color=clr)
                 embed.set_author(name=self.bot.user.display_name, url=self.bot.user.avatar_url, icon_url=self.bot.user.avatar_url)
                 embed.add_field(name="Compiled using:", value="```{}```".format(r.cc), inline=False)
                 embed.add_field(name="Result:", value="```{}```".format(r.cr), inline=False)
                 embed.set_footer(text="{} | {} | {}".format(r.uuid, host, chr))
             else:
-                embed=discord.Embed(title="Results of language {}".format(lang), color=0xee6d20)
+                embed=discord.Embed(title="Results of language {}".format(lang), color=clr)
                 embed.set_author(name=self.bot.user.display_name,url=self.bot.user.avatar_url,icon_url=self.bot.user.avatar_url)
                 embed.add_field(name="Compiled using:", value="```{}```".format(r.cc), inline=False)
                 embed.add_field(name="Compiler Result:", value="```{}```".format(r.ccr), inline=False)
@@ -50,7 +55,7 @@ class Containers(commands.Cog, name="Container Plugin"):
         error = getattr(error, 'original', error)
         if isinstance(error, commands.BadArgument):
             embed=discord.Embed(title="Error!", description="Unknown argument type or bad argument.", color=0xfd0000)
-            embed.add_field(name="Proper usage:", value="{}code <```amount CodeTokens> <language> <code>```".format(ctx.prefix), inline=True)
+            embed.add_field(name="Proper usage:", value="```{}code <amount CodeTokens> <language> <code>```".format(ctx.prefix), inline=True)
             embed.set_footer(text="HiggsBot - A code executing Discord bot!")
         if isinstance(error, commands.MissingRequiredArgument):
             embed=discord.Embed(title="Error!", description="Missing argument.", color=0xfd0000)
